5404e22d79678d1e78b6bdb8d97b5d18237079e6
Anton Mitkovets <amitkovets@ruelala.com>
2017-11-16 15:33:25 +0300

    Change hostname

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ SUDOKU_GRID_NUMBERS = int(os.environ.get('SUDOKU_GRID_NUMBERS', 35))
 if ENVIRONMENT == 'heroku':
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SERVER_PORT = os.environ['PORT']
-    URL = 'sudoku-everyday.herokuapp.com'
+    URL = 'trviad-sudoku.herokuapp.com'
 elif ENVIRONMENT == 'local':
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123456@localhost:5432/sudoku_tornado'
     SERVER_PORT = 8888
